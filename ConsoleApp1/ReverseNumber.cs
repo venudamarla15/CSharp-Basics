ce1024011981818e24ba52878bd7338b81dda17d
venudamarla15 <66801216+venudamarla15@users.noreply.github.com>
2024-07-08 19:01:14 -0500

    reverse number

---
+++ ConsoleApp1/ReverseNumber.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ReverseNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            int number = int.Parse(Console.ReadLine());
+
+            int remainder, reverse = 0;
+            int temp = number;
+
+            while (number > 0)
+            {
+                remainder = number % 10;
+
+                reverse = (reverse*10) + remainder;
+
+                number = number / 10;
+            }
+
+            Console.WriteLine("reverse number: "+ reverse);
+
+            Console.WriteLine($"{number}: given Number");
+
+            // check if the number is pallindrom 
+
+            if (temp == reverse)
+            {
+                Console.WriteLine($"{temp} is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{temp}: is not a palindrome");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Reversestring.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Reversestring
+    {
+        static void Main()
+        {
+            #region 
+            //forloop syntax
+
+            //Console.WriteLine("Enter string:");
+
+            //string Name = Console.ReadLine();
+
+            //string reverse = string.Empty;
+
+            //for (int i = Name.Length - 1; i >= 0; i--)
+            //{
+            //    reverse += Name[i];
+            //}
+            //Console.WriteLine(reverse);
+            //Console.ReadLine();
+            #endregion
+
+            #region
+            // foreach loop 
+            //Console.WriteLine("Enter string");
+            //string name = Console.ReadLine();
+            //string Reverse = string.Empty;
+            //foreach(var c in name)
+            //{
+            //    Reverse = c + Reverse;
+
+            //}
+            //Console.WriteLine(Reverse);
+            //Console.ReadLine();
+            #endregion
+
+            #region
+            // array.reverse method 
+            Console.WriteLine("Enter string:");
+             string name1 = Console.ReadLine();
+            char[] nameArray = name1.ToCharArray();
+            Array.Reverse(nameArray);   
+            string reverse = new string(nameArray);
+
+            Console.WriteLine(reverse);
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
