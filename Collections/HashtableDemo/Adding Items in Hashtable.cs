53f16f4d3e01ce26430f4906524c343050ba18ec
venudamarla15 <66801216+venudamarla15@users.noreply.github.com>
2024-07-24 09:24:56 -0500

    hashtable

---
+++ Collections/HashtableDemo/Adding Items in Hashtable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.HashtableDemo
+{
+    public class Adding_Items_in_Hashtable
+    {
+        static void Main()
+        {
+            Hashtable citiesHashTable = new Hashtable()
+            {
+                { "UK","London, paris, Manchester" },
+                {"India","Ap, Ts, Chennai, Hyd" },
+                {"USA", "Texas, Chicago, New York" }
+
+            };
+
+            Console.WriteLine("Creatign HashTable using collection-Initializer");
+
+            foreach (DictionaryEntry city in citiesHashTable)
+            {
+                Console.WriteLine($"Keys: {city.Key}, value:{city.Value}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
