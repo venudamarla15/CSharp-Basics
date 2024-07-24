d99c6f8073830641d6638982210dc8e529af352c
venudamarla15 <66801216+venudamarla15@users.noreply.github.com>
2024-07-24 12:35:47 -0500

    HashTable

---
+++ Collections2/HashtableDemo/CheckHashtable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections2.HashtableDemo
+{
+    public class CheckHashtable
+    {
+        static void Main()
+        {
+            Hashtable ht = new Hashtable();
+
+            ht.Add("Id", 101);
+            ht.Add("Name", "James"); //Here key is Name and value is James
+            ht.Add("Job", "Developer");
+            ht.Add("Salary", 3500);
+            ht.Add("Location", "Mumbai");
+            ht.Add("Dept", "IT");
+            ht.Add("EmailID", "a@a.com");
+
+            Console.WriteLine("Is Email key Exist : " + ht.Contains("Email"));
+            Console.WriteLine("Is Department exitst :" + ht.ContainsKey("Department"));
+            Console.WriteLine("Is Name Exist :" + ht.ContainsKey("Name"));
+            Console.WriteLine("Is Name value exist :" + ht.ContainsValue("James"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Collections2/HashtableDemo/CopyHashtable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections2.HashtableDemo
+{
+    public class CopyHashtable
+    {
+        static void  Main()
+        {
+            Hashtable ht = new Hashtable
+            {
+
+                { "EId", 1001 },
+                { "Name", "James" },
+                { "Salary", 3500 },
+                { "Location", "Mumbai" },
+                { "EmailID", "a@a.com" }
+            };
+
+            Console.WriteLine("HashTable Elements");
+            foreach(DictionaryEntry entry in ht)
+            {
+                Console.WriteLine($"Key:{entry.Key}, Value:{entry.Value}");
+
+            }
+            DictionaryEntry[] myArray = new DictionaryEntry [ht.Count];
+            ht.CopyTo (myArray, 0);
+            Console.WriteLine("\nHashtable Copy Array Elements:");
+            foreach (DictionaryEntry item in myArray)
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+            Object[] myObjArrayKey = new Object[ht.Count];
+            Object[] myObjArrayValue = new Object[ht.Count];
+            Console.WriteLine("\n Copy method to key");
+            ht.Keys.CopyTo(myObjArrayKey, 0);
+            foreach(var item in myObjArrayKey)
+            {
+                Console.WriteLine($"{item} ");
+            }
+            Console.WriteLine("\nCopyTo Method to Copy Values:");
+            ht.Values.CopyTo(myObjArrayValue, 0);
+            foreach (var key in myObjArrayValue)
+            {
+                Console.WriteLine($"{key} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
